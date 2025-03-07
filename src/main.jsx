74ff63b7ca598107b5d10103ae8760d26f85283e
Sabari1212 <sabarisankar1212@gmail.com>
2025-03-07 20:09:47 +0530

    PAssword

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Password from './components/Password.jsx'
 // npm create vite@latest
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    
+    {/* <App /> */}
+    <Password/>
   </StrictMode>,
 )
